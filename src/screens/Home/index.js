0e3d0b1449253aceb407599f88b2b4cce13f4b2c
David <l.rodrigues.david@gmail.com>
2020-03-31 19:16:50 +0200

    added ui for home page

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, Platform, StyleSheet, Text, Button } from 'react-native';
+import { View, Platform, StyleSheet, Text, Button, Image } from 'react-native';
 
 export default function Home(props) {
     const {navigation} = props;
@@ -9,15 +9,16 @@ export default function Home(props) {
 
 
 return <View style={styles.container}>
-    <Text style={styles.welcome}>Home</Text>
+    <Text style={styles.welcome}>Welcome into the Chuck Norris Application</Text>
+    <Image style={{height: 200, width: 300}} source={{uri: 'https://assets.chucknorris.host/img/chucknorris_logo_coloured_small@2x.png'}}/>
+    <Button onPress={onPresstoList} style={styles.enter} title="ENTER IN CHUCK NORRIS WORLD"/>
     <Text style={styles.platformText}>{platformText}</Text>
-    <Button onPress={onPresstoList} title="Go to List"/>
 </View>; 
 } 
 
 const platformText = Platform.select({
-    'ios': 'IOS device',
-    'android': 'Android device'
+    'ios': "You're in an IOS device, you're sutch a great man",
+    'android': "You're in an ANDROID device, nobody is judging you..."
 });
 
 const platformColor = Platform.select({
@@ -28,17 +29,23 @@ const platformColor = Platform.select({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         },
-        welcome: {
-        fontSize: 20,
-        textAlign: 'center',
+    welcome: {
+        fontSize: 40,
         margin: 10,
-        },
+        padding: 30,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
     platformText: {
-        fontSize: 24,
+        fontSize: 14,
         fontWeight: "bold",
-        color: platformColor
+        color: platformColor,
+        position: 'absolute', //Here is the trick
+        bottom: 50, //Here is the trick
+    },
+    enter: {
+        textDecorationLine: 'underline'
     }
 });
